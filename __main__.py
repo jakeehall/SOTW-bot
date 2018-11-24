90a8260f7fb7b8a498629da194bace982d6a40fa
Jake Hall <jakeehall@gmail.com>
2018-11-24 14:04:52 -0700

    v1.1.1
    * Uncommented addSongToHistory function in main
    * Removed setup.py

--- __main__.py
+++ __main__.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     from src.Database import Database
 
 
-    VERSION = "1.1.0" #Version of SOTW-bot (DO NOT CHANGE)
+    VERSION = "1.1.1" #Version of SOTW-bot (DO NOT CHANGE)
     # Select song, add it to history, and get its SOTW number
     db = Database(rbot.database) # Instantiate and connect to the database
     song = db.randomSong() # Randomly select new song
-    #db.addSongToHistory(song) # Add song to History and incriment times won
+    db.addSongToHistory(song) # Add song to History and incriment times won
     SOTWNumber = db.getSongOfTheWeekNumber() # Get the latest SOTW number
     del db # Close the database
 
