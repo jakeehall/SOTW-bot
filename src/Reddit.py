b486bb7c1eef4c21f421f0d0eead11078c8678af
Jake Hall <jakeehall@gmail.com>
2018-11-24 13:56:19 -0700

    v1.1.0
    * New Song Information Format (Table)
    * Links SOTW-Bot to GitHub page
    * Smaller SOTW-Bot Info at the bottom of Song Information Comment
    * Removed wrongly placed Settings_Template.py file in src directory

--- src/Reddit.py
+++ src/Reddit.py
@@ -30,15 +30,17 @@ class Reddit:
         post.mod.flair(text='SOTW #' + SOTWNumber, css_class=rbot.flairCSS)
 
         # Comment a Stickied Post with Moderator Distinction
-        description =   "*"+song.track+". "+song.title+"*\n\n" + \
-                        "*"+song.album+"*\n\n" + \
-                        "*"+song.artist+"*\n\n" + \
-                        "*"+song.year+"*\n\n" + \
+        description =   "||SOTW #"+SOTWNumber+"|\n" + \
+                        "|:-|:-|\n" + \
+                        "|Track|"+song.track+". "+song.title+"|\n" + \
+                        "|Album|"+song.album+"|\n" + \
+                        "|Artist|"+song.artist+"|\n" \
+                        "|Year|"+song.year+"|\n\n" + \
                         "Quote:\n" + \
                         song.quote+"\n\n" + \
                         "Credits:\n" + \
                         song.credits+"\n\n" + \
-                        "##### SOTW-bot v"+self.VERSION+" by u/JakeNation4"
+                        "^([SOTW-bot v"+self.VERSION+"](https://github.com/jakeehall/SOTW-bot) by [JakeNation4](https://www.reddit.com/user/JakeNation4))"
         post.reply(description).mod.distinguish(how='yes', sticky=True)
 
         # Print on Completion
--- src/Settings_Template.py
+++
@@ -1,9 +0,0 @@
-#SOTW-bot account must be a moderator of the given subreddit
-class RedditBot:
-    client_id = ''
-    client_secret = ''
-    username = ''
-    password=''
-    subreddit=''
-    flairCSS=''
-    database='database.db'
