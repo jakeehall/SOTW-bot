02785f806366e1bae25ed405cd178595c2fd154b
Jake Hall <jakeehall@gmail.com>
2018-12-01 23:31:53 -0700

    v1.2.0
    *The new Settings attributes, "historyEnabled" and "postingEnabled", are now displayed in the template "RedditBotTesting" Object, for developers to quickly toggle database history saving and Reddit posting off. To change to the "RedditBotTesting" Object quickly change "RedditBot" to "RedditBotTest" in __main__.py

--- Settings_Template.py
+++ Settings_Template.py
@@ -1,5 +1,19 @@
-#SOTW-bot account must be a moderator of the given subreddit
+# SOTW-bot account must be a moderator of the given subreddit
 class RedditBot:
+    client_id = ''
+    client_secret = ''
+    username = ''
+    password=''
+    subreddit=''
+    flairCSS=''
+    database='database.db'
+
+
+# FOR DEVELOPERS ONLY!
+# CHANGE "RedditBot" TO "RedditBotTest" IN __main__.py
+class RedditBotTest:
+    historyEnabled = False # SOTW history will not be saved to database
+    postingEnabled = False # SOTW will not be posted to Reddit
     client_id = ''
     client_secret = ''
     username = ''
