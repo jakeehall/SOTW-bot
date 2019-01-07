0f4deef04c5747036ab84d66a314a2928d24aaa9
Jake Hall <jakeehall@gmail.com>
2019-01-06 17:57:46 -0700

    v1.3.2
    *Testing on Python 3.5 in addition to Python 2.7
    *Removed outdated comment in "Settings_Template.py"

--- Settings_Template.py
+++ Settings_Template.py
@@ -10,7 +10,6 @@ class RedditBot:
 
 
 # FOR DEVELOPERS ONLY!
-# CHANGE "RedditBot" TO "RedditBotTest" IN __main__.py
 class RedditBotTest:
     historyEnabled = False # SOTW history will not be saved to database
     postingEnabled = False # SOTW will not be posted to Reddit
